5f6accb19001c20c6a2c2ec26f63c558d3b0bb47
TCwolfTC <fadyallemann@gmail.com>
2022-12-23 15:35:51 +0100

    Make the js get auto the screen size

--- Navbar.js
+++ Navbar.js
@@ -1,26 +1,26 @@
-let screensize = window.innerWidth;
-console.log(screensize)
 let pageButtons = document.getElementsByClassName('liststyle')
 let burgerButton = document.getElementById('burger')
-
-for (const pagebutton of pageButtons) {
-    pagebutton.style.display = 'none';
-    displaystyle = pagebutton.style.display;
-    console.log(displaystyle);
-}
-burgerButton.addEventListener('click', function () {
-    if (displaystyle == 'none') {
+document.querySelector('body').onresize = function () {
+    let w = document.documentElement.clientWidth;
+    console.log(w)
+    if (w < 800) {
         for (const pagebutton of pageButtons) {
-            pagebutton.style.display = 'flex';
-            displaystyle = pagebutton.style.display;
+            pagebutton.style.display = 'none';
         }
     } else {
         for (const pagebutton of pageButtons) {
-            pagebutton.style.display = 'none';
-            displaystyle = pagebutton.style.display;
+            pagebutton.style.display = 'flex';
         }
     }
-});
+
+    burgerButton.addEventListener('click', function () {
+
+        for (const pagebutton of pageButtons) {
+            pagebutton.style.display = 'flex';
+        }
+
+    });
 
 
 
+}
