6bacb85cf26d43f35abaf0e09d5d9591f32be2bf
TCwolfTC <fadyallemann@gmail.com>
2023-01-09 15:29:53 +0100

    Added the new navbar to html pages

--- Navbar.js
+++ Navbar.js
@@ -1,30 +1,28 @@
 let pageButtons = document.getElementsByClassName('liststyle')
 let burgerButton = document.getElementById('burger')
 document.querySelector('body').onresize = function () {
-    let w = document.documentElement.clientWidth;
-    if (w < 800) {
+    if (document.documentElement.clientWidth <= 800) {
         for (const pagebutton of pageButtons) {
             pagebutton.style.display = 'none';
         }
     } else {
+        
         for (const pagebutton of pageButtons) {
             pagebutton.style.display = 'flex';
         }
     }
 }
+
+
+
 burgerButton.addEventListener('click', function () {
-        for (const pagebutton of pageButtons) {
-             if (pagebutton.style.display == 'flex') {
-                pagebutton.style.display = 'none'
-
-
-             }else{
-                pagebutton.style.display = 'flex';
+    for (const pagebutton of pageButtons) {
+        if (pagebutton.style.display == 'flex') {
+                 pagebutton.style.display = 'none'
+        }else{
+                 pagebutton.style.display = 'flex';
              }
-        }
-        
-        
-        
-
-     
+    }
 });
+ 
+
