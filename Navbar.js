da9af7a9c84aae0887db509e5cb797bdff02196c
TCwolfTC <fadyallemann@gmail.com>
2023-01-09 13:40:12 +0100

    Make the burger button work

--- Navbar.js
+++ Navbar.js
@@ -2,7 +2,6 @@ let pageButtons = document.getElementsByClassName('liststyle')
 let burgerButton = document.getElementById('burger')
 document.querySelector('body').onresize = function () {
     let w = document.documentElement.clientWidth;
-    console.log(w)
     if (w < 800) {
         for (const pagebutton of pageButtons) {
             pagebutton.style.display = 'none';
@@ -12,15 +11,20 @@ document.querySelector('body').onresize = function () {
             pagebutton.style.display = 'flex';
         }
     }
-
-    burgerButton.addEventListener('click', function () {
-
-        for (const pagebutton of pageButtons) {
-            pagebutton.style.display = 'flex';
-        }
-
-    });
-
-
-
 }
+burgerButton.addEventListener('click', function () {
+        for (const pagebutton of pageButtons) {
+             if (pagebutton.style.display == 'flex') {
+                pagebutton.style.display = 'none'
+
+
+             }else{
+                pagebutton.style.display = 'flex';
+             }
+        }
+        
+        
+        
+
+     
+});
