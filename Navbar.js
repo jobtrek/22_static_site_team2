973521a12e862b75c4dec6c54e6d998c4d23a712
TCwolfTC <fadyallemann@gmail.com>
2023-01-10 14:36:04 +0100

    The navbar is done

--- Navbar.js
+++ Navbar.js
@@ -1,6 +1,8 @@
 let pageButtons = document.getElementsByClassName('liststyle')
 let burgerButton = document.getElementById('burger')
+let userScreen = document.querySelector('body').clientWidth; 
 document.querySelector('body').onresize = function () {
+    userScreen = document.querySelector('body').clientWidth;
     if (document.documentElement.clientWidth <= 800) {
         for (const pagebutton of pageButtons) {
             pagebutton.style.display = 'none';
@@ -11,17 +13,15 @@ document.querySelector('body').onresize = function () {
         }
     }
 }
-
-
-
 burgerButton.addEventListener('click', function () {
+    if (userScreen <= 800){
     for (const pagebutton of pageButtons) {
         if (pagebutton.style.display == 'flex') {
                  pagebutton.style.display = 'none'
         }else{
                  pagebutton.style.display = 'flex';
              }
+        }
     }
 });
  
-
